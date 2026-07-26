16163fefdd9d2bb19c52203537810f5dc794db1f
pythonP1rate <officialmadde@gmail.com>
2026-07-26 10:18:37 +0200

    hypr: add plugins config in Lua

---
+++ hypr/configs/plugins.lua
@@ -0,0 +1,26 @@
+-- ======================================================================
+-- Plugins Configuration
+-- ======================================================================
+
+-- All plugins are currently disabled.
+-- Uncomment and configure as needed.
+
+-- Hyprtrails
+-- hl.exec_once("hyprpm enable hyprtrails")
+
+-- Hyprexpo
+-- hl.exec_once("hyprpm enable hyprexpo")
+
+-- Hyprsplit
+-- hl.exec_once("hyprpm enable hyprsplit")
+
+-- Hyprtasking
+-- hl.exec_once("hyprpm enable hyprtasking")
+
+-- Hyprspace
+-- hl.exec_once("hyprpm enable hyprspace")
+
+-- Dynamic-cursors
+-- hl.exec_once("hyprpm enable dynamic-cursors")
+
+-- hl.exec_once("hyprpm reload -n")
